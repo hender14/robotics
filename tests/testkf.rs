bf43c481183b629c82a4ec7331a3765345055260
hender14 <work_cup_nakano@yahoo.co.jp>
2023-03-24 21:36:30 +0900

    fix: fix test parameter

--- tests/testkf.rs
+++ tests/testkf.rs
@@ -4,6 +4,7 @@ mod tests {
     use nalgebra as na;
     use robotics::kf::agent;
     use robotics::kf::kf;
+    use robotics;
     use std::f32::consts::PI;
 
     #[test]
@@ -40,16 +41,19 @@ mod tests {
         assert!(res, "\nans:{}  out:{}", &ans, &out);
     }
 
-    /* config landmark */
-    fn dec_landmark() -> (na::Matrix3<f32>, usize) {
-        let lpose1: na::RowVector3<f32> = na::RowVector3::new(-4., 2., 0.);
-        let lpose2: na::RowVector3<f32> = na::RowVector3::new(2., -3., 0.);
-        let lpose3: na::RowVector3<f32> = na::RowVector3::new(3., 3., 0.);
-        let lpose: na::Matrix3<f32> = na::Matrix3::from_rows(&[lpose1, lpose2, lpose3]);
-        let landsize = lpose.nrows();
-        println!("size:{}", landsize);
-        (lpose, landsize)
-    }
+/* config landmark */
+fn dec_landmark() -> (na::Matrix6x3<f32>, usize) {
+    let lpose1: na::RowVector3<f32> = na::RowVector3::new(-4., 2., 0.);
+    let lpose2: na::RowVector3<f32> = na::RowVector3::new(2., -3., 0.);
+    let lpose3: na::RowVector3<f32> = na::RowVector3::new(3., 3., 0.);
+    let lpose4: na::RowVector3<f32> = na::RowVector3::new(0., 4., 0.);
+    let lpose5: na::RowVector3<f32> = na::RowVector3::new(1., 1., 0.);
+    let lpose6: na::RowVector3<f32> = na::RowVector3::new(-3., -1., 0.);
+    let lpose: na::Matrix6x3<f32> = na::Matrix6x3::from_rows(&[lpose1, lpose2, lpose3, lpose4, lpose5, lpose6]);
+    let landsize = lpose.nrows();
+    println!("size:{}", landsize);
+    (lpose, landsize)
+}
 
     /* validate */
     fn validate(ans: &na::Vector3<f32>, out: &na::Vector3<f32>) -> bool {
